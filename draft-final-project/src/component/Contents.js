e54ae16271ad0cf7f93aa9a650507ad91b4951a8
fira <firaadani@gmail.com>
2021-01-24 12:51:59 +0700

    update styles

--- draft-final-project/src/component/Contents.js
+++ draft-final-project/src/component/Contents.js
@@ -1,131 +1,30 @@
 import React, { Component } from "react";
+import Thumbnail from "./Thumbnail";
 
 class Contents extends Component {
   render() {
     return (
-      <div>
-        <div className="post-container">
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/1.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/2.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/3.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/4.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/5.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/6.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/7.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/8.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/9.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/10.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/11.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/12.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/13.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/14.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-          <div className="post-thumbnail"><br />
-            <div className="post-img">
-              <img src="./images/safira/15.jpg" alt="" />
-            </div>
-            <div className="post-text">
-              <p>Insert Title Here</p>
-            </div>
-          </div>
-        </div>
+      <div className="post-container">
+        <Thumbnail imageExample="./images/safira/1.jpg" postText="ABNSC" />
+        <Thumbnail
+          imageExample="./images/safira/2.jpg"
+          postText="fdadfadfasdf"
+        />
+        <Thumbnail
+          imageExample="./images/safira/3.jpg"
+          postText="Aadsasdfasdfasdf"
+        />
+        <Thumbnail imageExample="./images/safira/4.jpg" postText="AasdsdSC" />
+        <Thumbnail
+          imageExample="./images/safira/5.jpg"
+          postText="ABdsdsdsdsdSC"
+        />
+        <Thumbnail imageExample="./images/safira/6.jpg" postText="AsdfsdSC" />
+        <Thumbnail imageExample="./images/safira/7.jpg" postText="ABdsdNSC" />
+        <Thumbnail imageExample="./images/safira/8.jpg" postText="ABsdfsdNSC" />
+        <Thumbnail imageExample="./images/safira/9.jpg" postText="ABNsdsdSC" />
+        <Thumbnail imageExample="./images/safira/10.jpg" postText="ABNsdsdSC" />
+        <Thumbnail imageExample="./images/safira/11.jpg" postText="ABNsdsdSC" />
       </div>
     );
   }
--- draft-final-project/src/component/Thumbnail.js
+++ draft-final-project/src/component/Thumbnail.js
@@ -1,26 +1,18 @@
 import React, { Component } from "react";
 
-export class Thumbnail extends Component {
+class Thumbnail extends Component {
   render() {
     return (
-      <div className="content">
-        <div className="wrap">
-          <div id="main" role="main">
-            <ul id="tiles">
-              <li onClick="lcoation.href='single-page.html':">
-                <img src="./images/img1.jpg" alt="brave" width="282" height="118" />
-                <div className="post-info">
-                  <div className="post-basic-ifo">
-                    <h3><a href="#">Animation films</a></h3>
-                    <span><a href="#"><label htmlFor=""></label>Movies</a></span>
-                    <p>Lorem ipsum is simply a dummy text of the printing and typesetting industry</p>
-                  </div>
-                </div>
-              </li>
-            </ul>
+      <div className="post-thumbnail">
+        <br />
+        <div className="post-img-text">
+          <div className="post-img">
+            <img src={this.props.imageExample} alt="" />
+          </div>
+          <div className="post-text">
+            <p>{this.props.postText}</p>
           </div>
         </div>
-        
       </div>
     );
   }
